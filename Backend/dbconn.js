53f78f83816e8b2b68398229c592d74a4e573e3b
ManmeetSingh777 <gmanmeetsingh888@gmail.com>
2024-05-13 15:45:56 +0530

    changes

---
+++ Backend/dbconn.js
@@ -0,0 +1,15 @@
+const mongoose=require('mongoose');
+
+const connectDb=async()=>{
+    try {
+        await mongoose.connect(process.env.DATABASE_URI, {
+            // useNewUrlParser: true,  //
+            // useUnifiedTopology: true ,//
+        })
+        console.log("Connected to mongodb");
+    }catch(error){
+        console.error("error connecting to mongodb:",error.message);
+    }
+}
+
+module.exports=connectDb
---
+++ Backend/server.js
@@ -0,0 +1,31 @@
+
+const express = require('express');
+const mongoose = require('mongoose');
+const connectDB = require('./dbconn');
+const router = require('./Route');
+require('dotenv').config();
+
+const app = express();
+app.use(express.json());
+
+app.get("/ping", (req, res) => {
+    res.send("pong");
+});
+app.use('/',router)
+const port = process.env.PORT || 8080;
+
+
+connectDB()
+    .then(() => {
+        console.log("Connected to MongoDB Database");
+        app.listen(port, (error) => {
+            if (error) {
+                console.error('Error starting the server:', error);
+            } else {
+                console.log(`The server is running on port ${port}`);
+            }
+        });
+    })
+    .catch((error) => {
+        console.error('Error connecting to MongoDB:', error);
+    });
