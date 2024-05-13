4966837cafdfa151acf69c5d6dfaf702293da75f
ManmeetSingh777 <gmanmeetsingh888@gmail.com>
2024-05-13 16:51:42 +0530

    done

--- Backend/dbconn.js
+++ Backend/dbconn.js
@@ -1,4 +1,6 @@
 const mongoose=require('mongoose');
+require('dotenv').config();
+
 
 const connectDb=async()=>{
     try {
--- Backend/server.js
+++ Backend/server.js
@@ -2,7 +2,7 @@
 const express = require('express');
 const mongoose = require('mongoose');
 const connectDB = require('./dbconn');
-const router = require('./Route');
+
 require('dotenv').config();
 
 const app = express();
@@ -11,7 +11,7 @@ app.use(express.json());
 app.get("/ping", (req, res) => {
     res.send("pong");
 });
-app.use('/',router)
+
 const port = process.env.PORT || 8080;
 
 
